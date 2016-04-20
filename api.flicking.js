534b4076d9efec277eced1eaf9e9ca4899d53803
Sung-min Yu <yu9221@gmail.com>
2016-04-20 10:18:01 +0900

    update

--- api.flicking.js
+++ api.flicking.js
@@ -88,7 +88,7 @@ transitionend: Chrome 1.0, Firefox 4.0 (2.0), Internet Explorer 10, Opera 10.5, 
 				for(key in options) {
 					if(!options.hasOwnProperty(key)) {
 						continue;
-					}else if(options[key] && typeof options[key] === 'object') {
+					}else if(options[key] && typeof options[key] === 'object' && !options[key].nodeType) {
 						settings[key] = this.setSettings(settings[key], options[key]);
 					}else {
 						settings[key] = options[key];
@@ -124,29 +124,27 @@ transitionend: Chrome 1.0, Firefox 4.0 (2.0), Internet Explorer 10, Opera 10.5, 
 		var that = this;
 		that.settings = {
 			'key': '',
-			'total': 0,
-			'width': 0,
-			'element': null,
-			'speed': 300,
-			'touch': true,
+			'total': 0, // 총 슬라이드 수
+			'width': 0, // 슬라이드 width 값
+			'element': null, // 슬라이드를 감싸고 있는 wrap
+			'speed': 300, // 슬라이드 속도
+			'touch': true, // 클릭 또는 터치 슬라이드 작동여부
+			'auto': 0, // 자동 슬라이드 작동여부 (0 이상의 값이 입력되면 작동합니다.)
 			'callback': null,
 			'transitionend': null
 		};
 		that.settings = module.setSettings(that.settings, settings);
 		that.translate = 0; // container 의 현재 translateX 값
 		that.index = 1; // 현재 출력되고 있는 슬라이드 (1부터 시작)
-		that.init();
-		if(that.settings.touch === true) {
-			that.on();
-		}
-		
-		// private init
-		/*(function() {
+		that.time = null; // 자동슬라이드 time key
 
-		})();*/
+		that.init();
+		that.on();
+		that.autoStart();
 	};
 	Flicking.prototype = {
 		init: function() {
+			var that = this;
 			this.start = {
 				"left": 0,
 				"top": 0,
@@ -157,169 +155,182 @@ transitionend: Chrome 1.0, Firefox 4.0 (2.0), Internet Explorer 10, Opera 10.5, 
 				"top": 0,
 				"time": 0
 			};
+			return that;
+		},
+		change: function(settings) {
+			var that = this;
+			return that;
 		},
 		on: function() {
 			var that = this;
+
 			that.off();
-
-			// down 이벤트
-			$(that['settings']['element']).on(env['event']['down'] + '.EVENT_MOUSEDOWN_flicking', function(e) {
-				//console.log('[정보] flicking MOUSEDOWN');
-				var event = e || window.event;
-				//var touch = (event.touches && event.touches[0]) || (event.changedTouches && event.changedTouches[0]);
-				var touch = event.touches; // touchstart
-
-				// 버블링(stopPropagation) 중지시키면, 상위 이벤트(예: document 에 적용된 이벤트)이 작동을 안한다.
-				// 현재 이벤트의 기본 동작을 중단한다. (터치 디바이스에서 기본 이벤트를 중단시키면 스크롤이 작동을 안한다. 모바일에서는 user-select: none; CSS로 해결한다.)
-				if(!env['check']['touch']) {
-					// PC에서는 마우스 이벤트 정확도(기능 정상작동)를 올리기 위해 정지
-					if(event.preventDefault) { 
-						event.preventDefault();
-					}else {
-						event.returnValue = false;
-					}
-				}
-				
-				// 멀티터치 방지
-				if(touch && touch.length && 1 < touch.length) {
-					return;
-				}
-
-				// 이벤트 정지
-				$(window).off('.EVENT_MOUSEMOVE_flicking');
-				$(window).off('.EVENT_MOUSEUP_flicking');
-				
-				// init
-				that.init();
-
-				// ---------- ---------- ---------- ---------- ---------- ----------
-				
-				// 시작값
-				if(touch) {
-					that['start']['left'] = touch[0].clientX;
-					that['start']['top'] = touch[0].clientY;
-				}else {
-					that['start']['left'] = event.clientX;
-					that['start']['top'] = event.clientY;
-				}
-				that['start']['time'] = new Date().getTime();
-				
-				// move 이벤트
-				$(window).on(env['event']['move'] + '.EVENT_MOUSEMOVE_flicking', function(e) {
-					//console.log('[정보] flicking MOUSEMOVE');
+			if(that.settings.touch === true) {
+				// down 이벤트
+				$(that['settings']['element']).on(env['event']['down'] + '.EVENT_MOUSEDOWN_flicking', function(e) {
+					//console.log('[정보] flicking MOUSEDOWN');
 					var event = e || window.event;
 					//var touch = (event.touches && event.touches[0]) || (event.changedTouches && event.changedTouches[0]);
-					var touch = event.touches || event.changedTouches;
-					var left, top, translate;
+					var touch = event.touches; // touchstart
 
-					// 이동값
-					if(touch) {
-						left = touch[0].clientX;
-						top = touch[0].clientY;
-					}else {
-						left = event.clientX;
-						top = event.clientY;
-					}
-
-					// 로그
-					/*console.log("left: " + left);
-					console.log("that['start']['left']: " + that['start']['left']);
-					console.log("top: " + top);
-					console.log("that['start']['top']: " + that['start']['top']);
-					console.log(Math.abs(left - that['start']['left']));
-					console.log(Math.abs(top - that['start']['top']));*/
-
-					// 사용자 터치가 스크롤인지 슬라이드인지 확인하여 안정화함
-					if(Math.abs(left - that['start']['left']) > Math.abs(top - that['start']['top'])) { 
-						// 현재 이벤트의 기본 동작을 중단한다. (슬라이드가 작동중일 때 모바일의 기본이벤트인 스크롤 작동을 중단시킨다.)
+					// 버블링(stopPropagation) 중지시키면, 상위 이벤트(예: document 에 적용된 이벤트)이 작동을 안한다.
+					// 현재 이벤트의 기본 동작을 중단한다. (터치 디바이스에서 기본 이벤트를 중단시키면 스크롤이 작동을 안한다. 모바일에서는 user-select: none; CSS로 해결한다.)
+					if(!env['check']['touch']) {
+						// PC에서는 마우스 이벤트 정확도(기능 정상작동)를 올리기 위해 정지
 						if(event.preventDefault) { 
 							event.preventDefault();
 						}else {
 							event.returnValue = false;
 						}
-						// slide 이동
-						translate = (left - that['start']['left']) + that['translate'];
-						module.setTranslate({'duration': 0, 'translateX': translate, 'element': that['settings']['element']});
 					}
-				});
-				
-				// up 이벤트
-				$(window).on(env['event']['up'] + '.EVENT_MOUSEUP_flicking', function(e) {
-					//console.log('[정보] flicking MOUSEUP');
-					var event = e || window.event;
-					//var touch = (event.touches && event.touches[0]) || (event.changedTouches && event.changedTouches[0]);
-					var touch = event.changedTouches; // touchend
-					var time;
-					var left, top;
-					var index, duration;
-
-					// 현재 이벤트의 기본 동작을 중단한다.
-					if(event.preventDefault) { 
-						event.preventDefault();
-					}else {
-						event.returnValue = false;
+					
+					// 멀티터치 방지
+					if(touch && touch.length && 1 < touch.length) {
+						return;
 					}
-
-					// 정지값
-					if(touch) {
-						that['end']['left'] = touch[0].clientX;
-						that['end']['top'] = touch[0].clientY;
-					}else {
-						that['end']['left'] = event.clientX;
-						that['end']['top'] = event.clientY;
-					}
-					that['end']['time'] = new Date().getTime();
-
-					time = Number(that['end']['time']) - Number(that['start']['time']);
-					left = that['end']['left'] - that['start']['left'];
-					top = that['end']['top'] - that['start']['top'];
-					index = that['index'];
-					duration = Number(that['settings']['speed']) / 1000; /* 300 / 1000 */
-
-					// 이동 가능한지 검사
-					if(Math.abs(left) > Math.abs(top) && ((time <= 100 && 30 <= Math.abs(left))/*마우스를 빠르게 이동한 경우*/ || (that['settings']['width'] / 6) < Math.abs(left)/*기준값 이상 이동한 경우*/)) {
-						if(index < that['settings']['total'] && left < 0) { // 다음
-							index++;
-						}else if(1 < index && left > 0) { // 이전
-							index--;
-						}
-						// 슬라이드 속도
-						duration = (time > 1000 || 6000 < time) ? duration : (time+100) / 1000;
-					}
-
-					/*
-					// 로그
-					console.log('[실행정보] 플로킹');
-					console.log('index: ' + that['index']);
-					console.log('index: ' + index);
-					console.log('duration: ' + duration);
-					console.dir(that['start']);
-					console.dir(that['end']);
-					*/
-
-					// 슬라이드 이동 (transitionend 이벤트 발생됨)
-					that.slide({'value': index, 'duration': duration});
 
 					// 이벤트 정지
 					$(window).off('.EVENT_MOUSEMOVE_flicking');
 					$(window).off('.EVENT_MOUSEUP_flicking');
 
+					// auto
+					that.autoStop();
+					
 					// init
 					that.init();
-				});
-			});
 
-			// 트랜지션 (하위 자식 노드의 transition 전파에 따라 실행될 수 있다. 자식의 transition 전파를 막으려면 해당 자식 이벤트에 stopPropagation 실행)
-			if(typeof that['settings']['transitionend'] === 'function') {
-				$(that['settings']['element']).on(env['event']['transitionend'] + '.EVENT_TRANSITION_flicking', function(e) {
-					var event = e || window.event;
-					if(that['settings']['element'].isEqualNode(event.target)) {
-						that['settings']['transitionend'].call(that['settings']['element'], that['index']);
+					// ---------- ---------- ---------- ---------- ---------- ----------
+					
+					// 시작값
+					if(touch) {
+						that['start']['left'] = touch[0].clientX;
+						that['start']['top'] = touch[0].clientY;
+					}else {
+						that['start']['left'] = event.clientX;
+						that['start']['top'] = event.clientY;
 					}
+					that['start']['time'] = new Date().getTime();
+					
+					// move 이벤트
+					$(window).on(env['event']['move'] + '.EVENT_MOUSEMOVE_flicking', function(e) {
+						//console.log('[정보] flicking MOUSEMOVE');
+						var event = e || window.event;
+						//var touch = (event.touches && event.touches[0]) || (event.changedTouches && event.changedTouches[0]);
+						var touch = event.touches || event.changedTouches;
+						var left, top, translate;
+
+						// 이동값
+						if(touch) {
+							left = touch[0].clientX;
+							top = touch[0].clientY;
+						}else {
+							left = event.clientX;
+							top = event.clientY;
+						}
+
+						// 로그
+						/*console.log("left: " + left);
+						console.log("that['start']['left']: " + that['start']['left']);
+						console.log("top: " + top);
+						console.log("that['start']['top']: " + that['start']['top']);
+						console.log(Math.abs(left - that['start']['left']));
+						console.log(Math.abs(top - that['start']['top']));*/
+
+						// 사용자 터치가 스크롤인지 슬라이드인지 확인하여 안정화함
+						if(Math.abs(left - that['start']['left']) > Math.abs(top - that['start']['top'])) { 
+							// 현재 이벤트의 기본 동작을 중단한다. (슬라이드가 작동중일 때 모바일의 기본이벤트인 스크롤 작동을 중단시킨다.)
+							if(event.preventDefault) { 
+								event.preventDefault();
+							}else {
+								event.returnValue = false;
+							}
+							// slide 이동
+							translate = (left - that['start']['left']) + that['translate'];
+							module.setTranslate({'duration': 0, 'translateX': translate, 'element': that['settings']['element']});
+						}
+					});
+					
+					// up 이벤트
+					$(window).on(env['event']['up'] + '.EVENT_MOUSEUP_flicking', function(e) {
+						//console.log('[정보] flicking MOUSEUP');
+						var event = e || window.event;
+						//var touch = (event.touches && event.touches[0]) || (event.changedTouches && event.changedTouches[0]);
+						var touch = event.changedTouches; // touchend
+						var time;
+						var left, top;
+						var index, duration;
+
+						// 현재 이벤트의 기본 동작을 중단한다.
+						if(event.preventDefault) { 
+							event.preventDefault();
+						}else {
+							event.returnValue = false;
+						}
+
+						// 정지값
+						if(touch) {
+							that['end']['left'] = touch[0].clientX;
+							that['end']['top'] = touch[0].clientY;
+						}else {
+							that['end']['left'] = event.clientX;
+							that['end']['top'] = event.clientY;
+						}
+						that['end']['time'] = new Date().getTime();
+
+						time = Number(that['end']['time']) - Number(that['start']['time']);
+						left = that['end']['left'] - that['start']['left'];
+						top = that['end']['top'] - that['start']['top'];
+						index = that['index'];
+						duration = Number(that['settings']['speed']) / 1000; /* 300 / 1000 */
+
+						// 이동 가능한지 검사
+						if(Math.abs(left) > Math.abs(top) && ((time <= 100 && 30 <= Math.abs(left))/*마우스를 빠르게 이동한 경우*/ || (that['settings']['width'] / 6) < Math.abs(left)/*기준값 이상 이동한 경우*/)) {
+							if(index < that['settings']['total'] && left < 0) { // 다음
+								index++;
+							}else if(1 < index && left > 0) { // 이전
+								index--;
+							}
+							// 슬라이드 속도
+							duration = (time > 1000 || 6000 < time) ? duration : (time+100) / 1000;
+						}
+
+						/*
+						// 로그
+						console.log('[실행정보] 플로킹');
+						console.log('index: ' + that['index']);
+						console.log('index: ' + index);
+						console.log('duration: ' + duration);
+						console.dir(that['start']);
+						console.dir(that['end']);
+						*/
+
+						// 슬라이드 이동 (transitionend 이벤트 발생됨)
+						that.slide({'value': index, 'duration': duration});
+
+						// 이벤트 정지
+						$(window).off('.EVENT_MOUSEMOVE_flicking');
+						$(window).off('.EVENT_MOUSEUP_flicking');
+
+						// auto
+						that.autoStart();
+
+						// init
+						that.init();
+					});
 				});
+
+				// 트랜지션 (하위 자식 노드의 transition 전파에 따라 실행될 수 있다. 자식의 transition 전파를 막으려면 해당 자식 이벤트에 stopPropagation 실행)
+				if(typeof that['settings']['transitionend'] === 'function') {
+					$(that['settings']['element']).on(env['event']['transitionend'] + '.EVENT_TRANSITION_flicking', function(e) {
+						var event = e || window.event;
+						if(that['settings']['element'].isEqualNode(event.target)) {
+							that['settings']['transitionend'].call(that['settings']['element'], that['index']);
+						}
+					});
+				}
 			}
 
-			return true;
+			return that;
 		},
 		off: function() {
 			var that = this;
@@ -327,8 +338,7 @@ transitionend: Chrome 1.0, Firefox 4.0 (2.0), Internet Explorer 10, Opera 10.5, 
 			$(window).off('.EVENT_MOUSEUP_flicking');
 			$(that['settings']['element']).off('.EVENT_MOUSEDOWN_flicking');
 			$(that['settings']['element']).off('.EVENT_TRANSITION_flicking');
-
-			return true;
+			return that;
 		},
 		slide: function(parameter) {
 			var that = this;
@@ -374,7 +384,27 @@ transitionend: Chrome 1.0, Firefox 4.0 (2.0), Internet Explorer 10, Opera 10.5, 
 				that['settings']['callback'].call(that['settings']['element'], that['index']);
 			}
 
+			// auto
+			that.autoStart();
+
 			return true;
+		},
+		autoStart: function() {
+			var that = this;
+			that.autoStop();
+			if(that.settings.auto > 0) {
+				that.time = window.setTimeout(function() {
+					if(that['index'] < that['settings']['total']) {
+						that.slide({'value': 'next'});
+					}else {
+						that.slide({'value': 1});
+					}					
+				}, that.settings.auto);
+			}
+		},
+		autoStop: function() {
+			var that = this;
+			window.clearTimeout(that.time);
 		}
 	};
 
@@ -385,6 +415,7 @@ transitionend: Chrome 1.0, Firefox 4.0 (2.0), Internet Explorer 10, Opera 10.5, 
 			var instance;
 			if(settings['key'] && module.instance[settings['key']]) {
 				instance = module.instance[settings['key']];
+				instance.change(settings);
 			}else {
 				instance = new Flicking(settings);
 				if(settings['key']) {
