f01e7842b15dc649057516dc965e5a376dce9e80
유성민 <sungmin.yu@cj.net>
2018-09-19 17:45:12 +0900

    update

--- public/build/api.animate.js
+++ public/build/api.animate.js
@@ -109,6 +109,31 @@ jQuery 또는 api.dom 에 종속적 실행
 		})();
 	}
 
+	// 정규식
+	var regexp = {
+		pixel_unit_list: /width$|height$|top|right|bottom|left|fontSize|letterSpacing|lineHeight|^margin*|^padding*/i, // 단위 px 해당되는 것
+		time_unit_list: /.+(-duration|-delay)$/i, // seconds (s) or milliseconds (ms)
+		position_list: /^(top|right|bottom|left)$/,
+		display_list: /^(display|visibility|opacity|)$/i,
+		text: /^(\D+)$/i, // 텍스트
+		num_unit: /^([0-9]+)(\D+)$/i, // 단위
+		num: /^[+-]?\d+(\.\d+)?$/, // 숫자
+		source_num: /[+-]?(?:\d*\.|)\d+(?:[eE][+-]?\d+|)/.source, // +=, -= 숫자와 연산자 분리
+		trim: /(^\s*)|(\s*$)/g // 양쪽 여백
+	};
+
+	// 숫자/단위 분리 (예: 10px -> [0]=>10px, [1]=>10, [2]=>'px')
+	var getNumberUnit = function(value) {
+		//var regexp_source_num = /[+-]?(?:\d*\.|)\d+(?:[eE][+-]?\d+|)/.source;
+		var regexp_number = new RegExp("^(" + regexp.source_num + ")(.*)$", "i");
+		var matches = regexp_number.exec(value);
+		if(matches) {
+			return matches;
+		}else {
+			return [value];
+		}
+	};
+
 	// requestAnimationFrame, cancelAnimationFrame
 	var setRequestAnimationFrame = (function() { 
 		// https://developer.mozilla.org/en-US/docs/Web/API/Window/requestAnimationFrame
