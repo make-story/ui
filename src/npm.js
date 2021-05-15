a047f068a7931c18c160d0abf44424db77195022
Sung-min Yu <yu9221@gmail.com>
2021-05-15 16:38:55 +0900

    update

---
+++ src/npm.js
@@ -0,0 +1,10 @@
+/**
+ * npm 배포
+ */
+const execSync = require('child_process').execSync;
+
+// 배포 리스트
+const list = [];
+
+// 쉘 명령 실행결과 반환
+console.log(execSync("echo 'test'", { encoding: 'UTF-8' }));
