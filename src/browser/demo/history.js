6f9e1f9cbb98551690fb4c7e58afdcdb702b1574
Sung-min Yu <yu9221@gmail.com>
2021-08-26 21:13:21 +0900

    update

--- src/browser/demo/history.js
+++ src/browser/demo/history.js
@@ -26,31 +26,31 @@ setScrollRestoration('manual');
 
 // BFCache 에 따른 새로고침 전 스크롤값 저장
 const HISTORY_BFCACHE_SCROLL = 'HISTORY_BFCACHE_SCROLL';
-const setHistoryBFCacheScroll = (key = HISTORY_BFCACHE_SCROLL) => {
+const setHistoryBFCacheScroll = (key = HISTORY_BFCACHE_SCROLL) => { // 공통
   window.sessionStorage.setItem(key, JSON.stringify(getHistoryWindowScroll()));
 };
-const getHistoryBFCacheScroll = () => {
+const getHistoryBFCacheScroll = () => { // 공통
   return JSON.parse(window.sessionStorage.getItem(HISTORY_BFCACHE_SCROLL) || '{}');
 };
 
 // 페이지정보 저장 (IOS 14 버전이상에서 뒤로가기/앞으로가기 후 자동 새로고침 이슈 대응하기 위함)
 const HISTORY_LOCATION = 'HISTORY_LOCATION';
-const setHistoryLocation = (key = HISTORY_LOCATION) => {
+const setHistoryLocation = (key = HISTORY_LOCATION) => { // 공통
   window.sessionStorage.setItem(key, window.location.href);
 };
-const getHistoryLocation = (key = HISTORY_LOCATION) => {
+const getHistoryLocation = (key = HISTORY_LOCATION) => { // 공통
   return window.sessionStorage.getItem(key);
 };
 const HISTORY_RELOAD_SCROLL = 'HISTORY_RELOAD_SCROLL';
-const setHistoryReloadScroll = (key = HISTORY_RELOAD_SCROLL) => {
+const setHistoryReloadScroll = (key = HISTORY_RELOAD_SCROLL) => { // 공통
   window.sessionStorage.setItem(key, JSON.stringify(getHistoryWindowScroll()));
 };
-const getHistoryReloadScroll = (key = HISTORY_RELOAD_SCROLL) => {
+const getHistoryReloadScroll = (key = HISTORY_RELOAD_SCROLL) => { // 공통
   return JSON.parse(window.sessionStorage.getItem(key) || '{}');
 };
 
 // 브라우저 스크롤 이동
-const setWindowScroll = (navigationType) => {
+const setNavigationTypeWindowScroll = (navigationType) => { // 공통
   let top = 0;
   if (window.history.scrollRestoration && window.history.scrollRestoration !== 'manual') {
     // 브라우저 히스토리 스크롤 수동제어 모드가 아닌 경우
@@ -64,13 +64,13 @@ const setWindowScroll = (navigationType) => {
     top = Number(getHistoryReloadScroll().top) || 0;
   }
   if (0 < top) {
-    console.log('setWindowScroll', navigationType, top);
+    console.log('setNavigationTypeWindowScroll', navigationType, top);
     window.scrollTo({ top, behavior: 'auto' });
   }
 };
 
 // pageshow 콜백
-const setPageShowCallback = ((browserName) => {
+const setPageShowCallback = ((browserName) => { // 내부적 사용
   const isSafari = browserName && -1 < browserName.toLowerCase().indexOf('safari');
   let timePageShowTimeout = 0;
   return (navigationType) => {
@@ -83,7 +83,7 @@ const setPageShowCallback = ((browserName) => {
     } else {
       timePageShowTimeout = window.setTimeout(
         () => {
-          setWindowScroll(navigationType);
+          setNavigationTypeWindowScroll(navigationType);
         },
         isSafari ? 350 : 0,
       ); // Next.js Route 실행으로 페이지 최상단 이동되는 것 이후 실행 (사파리 등에서 Next routeChangeStart, routeChangeComplete 등 실행됨)
@@ -92,17 +92,17 @@ const setPageShowCallback = ((browserName) => {
 })(getBrowserName());
 
 // DOM Ready 콜백
-const setDOMReadyCallback = () => {
-  setWindowScroll(getNavigationType());
+const setDOMReadyCallback = () => { // 내부적 사용
+  setNavigationTypeWindowScroll(getNavigationType());
 };
 
 // 사용자 터치가 발생하면, 히스토리 스크롤 이동 정지
-const setUserTouchListener = (event) => {
+const setUserTouchListener = (event) => { // 내부적 사용
   //console.log(event);
   isPageShowCallbackCancel(setPageShowCallback);
   isDOMReadyCallbackCancel(setDOMReadyCallback);
 };
-const setUserTouchWatch = (listener) => {
+const setUserTouchWatch = (listener) => { // 공통
   window.document?.body?.removeEventListener('touchstart', listener);
   window.document?.body?.removeEventListener('touchmove', listener);
   window.document?.body?.addEventListener('touchstart', listener, { once: true });
@@ -110,7 +110,7 @@ const setUserTouchWatch = (listener) => {
 };
 
 // 히스토리
-const setHistoryPageListener = ((browserName) => {
+const setHistoryPageListener = ((browserName) => { // 내부적 사용
   // 클로저 - node js 에서 메모리 누수에 주의!
   const isSafari = browserName && -1 < browserName.toLowerCase().indexOf('safari');
   let timeHistoryPageInterval = 0;
@@ -133,13 +133,13 @@ const setHistoryPageListener = ((browserName) => {
     setHistoryWindowScroll();
   };
 })(getBrowserName());
-const setHistoryPage = (listener) => {
+const setHistoryPage = (listener) => { // 내부적 사용
   // 사용자가 페이지를 떠날 때
   // unload (beforeunload 이벤트는 제외) 사용하지 않은 이유 : 브라우저는 페이지에 unload 이벤트 리스너가 추가되어 있는 경우, bfcache에 적합하지 않은 페이지로 판단하는 경우가 많다.
   window.removeEventListener('pagehide', listener);
   window.addEventListener('pagehide', listener), { once: true };
 };
-const setHistoryCheck = (browserName) => {
+const setHistoryCheck = (browserName) => { // 내부적 사용
   console.log('setHistoryCheck', browserName);
   //const isSafari = browserName && -1 < browserName.toLowerCase().indexOf('safari');
   /*if (isSafari) {
